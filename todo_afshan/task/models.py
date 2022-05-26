3851f25656ecd9ca11b1b69eef550883c1e20909
Mohammad <mohammadbabaeesem@gmail.com>
2022-05-26 10:00:47 +0430

    Change Task model

--- todo_afshan/task/models.py
+++ todo_afshan/task/models.py
@@ -1,13 +1,12 @@
 from datetime import datetime
-from pyexpat import model
-from statistics import mode
 from django.db import models
 
 # Create your models here.
 
 
 class Task(models.Model):
-    title = models.CharField(max_length=50)
+    title = models.CharField(max_length=20, blank=False, null=False)
     description = models.TextField(blank=True)
-    creation_date = models.TimeField(default=datetime.now())
-    todo_date = models.DateTimeField()
+    creation_date = models.DateTimeField(default=datetime.now())
+    todo_date = models.DateTimeField(blank=False,null=False)
+    

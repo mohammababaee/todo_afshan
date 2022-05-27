ac219bdd9cfb7e21722f2454a24c35a45a779c2c
Mohammad <mohammadbabaeesem@gmail.com>
2022-05-27 19:31:49 +0430

    Add django admin for apps

--- todo_afshan/todo_afshan/urls.py
+++ todo_afshan/todo_afshan/urls.py
@@ -20,6 +20,6 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/task', include('task.urls')),
-    path('api/v1/user', include('user.urls'))
+    #path('api/v1/task', include('task.urls')),
+    #path('api/v1/user', include('user.urls'))
 ]

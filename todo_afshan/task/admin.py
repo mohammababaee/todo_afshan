dd7f6f5a52e7136b0632feff138b01b0c97bd3cf
Mohammad <mohammadbabaeesem@gmail.com>
2022-06-16 12:14:27 +0430

    Update django admin

--- todo_afshan/task/admin.py
+++ todo_afshan/task/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 from .models import Task
 # Register your models here.
 
-admin.site.register(Task)
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['title', 'description', 'todo_date']
+    sortable_by = ['todo_date', 'title']
+    list_editable = ['todo_date']
+    search_fields = ['title']
--- todo_afshan/user/admin.py
+++ todo_afshan/user/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import CustomUser
 # Register your models here.
 
-admin.site.register(CustomUser)
+@admin.register(CustomUser)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['username','is_staff','is_active']
+    list_editable = ['is_staff']
+    readonly_fields = ['username','password']
